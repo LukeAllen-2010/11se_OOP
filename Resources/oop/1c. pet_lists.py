cbacc144ae778a96fbc4f12e744f04adc0f56660
LukeAllen-2010 <lukeallen152@gmail.com>
2026-05-06 23:07:45 +0000

    sync changes

--- Resources/oop/1c. pet_lists.py
+++ Resources/oop/1c. pet_lists.py
@@ -1,9 +1,9 @@
 #Tutorial 3 Lists:
 #1. Create an example of parallel lists eg: pet_name, species, age, vaccination_status for three pets
 pets = []
-pets.append(('Bonnie', 'cat', 3, False))
-pets.append(('Ben', 'monkey', 25, False))
-pets.append(('Luke', 'dog', 15, True))
+pets.append(['Bonnie', 'cat', 3, False])
+pets.append(['Ben', 'monkey', 25, False])
+pets.append(['Luke', 'dog', 15, True])
 
 #2. Use a for loop to print parallel list details. This will mean that one complete printout will look like:
 '''
@@ -26,11 +26,16 @@ print_pets(pets)
   #ACTIVITIES:
 # In each activity test out that the printing of data is still valid
 #1. Add a new animal named Hootie, its a blowfish, it is 34 years
-pets.append(('Hootie', 'blowfish', 34, False))
+pets.append(['Hootie', 'blowfish', 34, False])
 
 #2. Vaccinate an unvaccinated animal (create vaccination)
 def vaccinate(pet):
    pet[3] = True
 
-vaccinate(pets[3])
+vaccinate(pets[2])
 #3. Remove an animal and make sure that all the printing is correct
+def remove_pet(list, pet_number):
+  pets.remove(list[pet_number])
+
+remove_pet(pets, 2)
+print(pets)
