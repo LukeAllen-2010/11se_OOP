cdb075a4ccb3ecf9e579a84e1daecdf84e2f4522
LukeAllen-2010 <lukeallen152@gmail.com>
2026-05-11 00:15:57 +0000

    update 5a

--- Resources/oop/5a._fighter_class_activities.py
+++ Resources/oop/5a._fighter_class_activities.py
@@ -6,8 +6,28 @@
 import random
 
 class Fighter:
-    def __init__(self, name):
+    def __init__(self, name, strength, weapon = None):
         self.name = name
+        self.strength = strength
+        self.weapon = weapon
+        self.health = 100
     
+    def get_weapon_damage(self):
+        if self.weapon == 'sword':
+            return 10
+        elif self.weapon == 'club':
+            return 6
+        elif self.weapon == 'halberd':
+            return 8
+
     def slime_out(self):
-        
+        attack = self.strength + self.get_weapon_damage + random.randint(-5, 8)
+        return attack
+
+player1 = Fighter('Benjamin', 4, 'club')
+player2 = Fighter('Bartholemew the Worthy', 10, 'sword')
+
+if random.randint(1, 2) == 1:
+    player2.health -= player1.slime_out()
+else:
+    player1.health -= player2.slime_out()
