89ea2d53e99fd773573fa0b54f9c73ffaf857f83
LukeAllen-2010 <lukeallen152@gmail.com>
2026-05-18 00:33:37 +0000

    finalise fighter

--- Mini Projects/hello-world.py
+++ Mini Projects/hello-world.py
@@ -1,3 +1,6 @@
 import random
-list = ["apple", "banana", "pear", "help me", ]
+list = ["apple", "banana", "pear", "help me"]
 print("Hello", random.choice(list))
+def function():
+    print('help_me')
+print(type(function))
