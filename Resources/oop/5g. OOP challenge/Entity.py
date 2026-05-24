1c94fad24b38ee3cc143a6cfe6ac3d661196dbf9
LukeAllen-2010 <lukeallen152@gmail.com>
2026-05-24 23:40:05 +0000

    update 5g

--- Resources/oop/5g. OOP challenge/Entity.py
+++ Resources/oop/5g. OOP challenge/Entity.py
@@ -1,11 +1,9 @@
 import random
-from Hero import Attack
-
+from Main import Weapon
 class Entity:
     def __init__(self, name:str, starting_health:int, weapon:str, armour_class:int, strength:int, dexterity:int, constitution:int, intelligence:int, wisdom:int, charisma:int, gp=0, sp=0, cp=0):
         self.name = name
         self.__health = starting_health
-        self.weapon = Weapon()
         # self.weapon_name = weapon
         # self.weapon_dice = WEAPON_MAPPING[weapon]
         self.abilities = {'strength':strength, 'dexterity':dexterity, 'constitution':constitution, 'intelligence':intelligence, 'wisdom':wisdom, 'charisma':charisma}
@@ -23,11 +21,26 @@ class Entity:
             ability_modifier = self.get_ability_modifier(self.abilities['strength'])
         else:
             ability_modifier = self.get_ability_modifier(self.abilities['dexterity'])
-        self.attacks[attack_name] = Attack(attack_name, dice_amount, dice_type, ability_modifier)
+        self.attacks[attack_name] = Weapon(attack_name, dice_amount, dice_type, ability_modifier)
 
     def get_ability_modifier(self, ability_score):
         return (ability_score - 10) // 2
     
     def get_proficiency_bonus(self):
         return (self.level - 1) // 4 + 2
+    
+WEAPON_MAPPING = {
+    'dagger' : (1,4),
+    'club' : (1,4),
+    'spear' : (1,6),
+    'greatsword' : (2,6),
+    'halberd' : (1,10),
+    'rapier' : (1,8)
+}
+
+hero = Entity('Gimli', 100, 'halberd', 12, 10, 10, 10, 10, 10, 10)
+hero.add_attack('Halberd', WEAPON_MAPPING['halberd'][0], WEAPON_MAPPING['halberd'][1], True)
+
+
+print(hero.attacks)
 
