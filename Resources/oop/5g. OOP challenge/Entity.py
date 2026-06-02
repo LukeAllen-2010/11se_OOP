c0106996aa342331ab8b35f41990fad9b2408cce
LukeAllen-2010 <lukeallen152@gmail.com>
2026-06-02 05:26:04 +0000

    update game

--- Resources/oop/5g. OOP challenge/Entity.py
+++ Resources/oop/5g. OOP challenge/Entity.py
@@ -1,46 +1,70 @@
 import random
-from Main import Weapon
+
+
+
 class Entity:
-    def __init__(self, name:str, starting_health:int, weapon:str, armour_class:int, strength:int, dexterity:int, constitution:int, intelligence:int, wisdom:int, charisma:int, gp=0, sp=0, cp=0):
+    def __init__(self, name:str, starting_health:int, player_attacks:list, armour_class:int, strength:int, dexterity:int, constitution:int=0, intelligence:int=0, wisdom:int=0, charisma:int=0, gp=0, sp=0, cp=0):
         self.name = name
         self.__health = starting_health
-        # self.weapon_name = weapon
-        # self.weapon_dice = WEAPON_MAPPING[weapon]
         self.abilities = {'strength':strength, 'dexterity':dexterity, 'constitution':constitution, 'intelligence':intelligence, 'wisdom':wisdom, 'charisma':charisma}
+        self.purse = {'gold':gp, 'silver':sp, 'copper':cp}
         self.ac = armour_class
         self.attacks = {}
+        for attack_name in player_attacks:
+            self.attacks[attack_name] = ATTACKS[attack_name]
 
     def get_roll(self, dice_amount, dice_type):
         roll = 0
         for i in range(dice_amount):
             roll += random.randint(1, dice_type)
         return roll
-
-    def add_attack(self, attack_name:str, dice_amount, dice_type, melee):
-        if melee:
-            ability_modifier = self.get_ability_modifier(self.abilities['strength'])
-        else:
-            ability_modifier = self.get_ability_modifier(self.abilities['dexterity'])
-        self.attacks[attack_name] = Weapon(attack_name, dice_amount, dice_type, ability_modifier)
-
-    def get_ability_modifier(self, ability_score):
-        return (ability_score - 10) // 2
+    
+    def get_ability_modifier(self, ability_type):
+        return (self.abilities[ability_type] - 10) // 2
     
     def get_proficiency_bonus(self):
         return (self.level - 1) // 4 + 2
     
-WEAPON_MAPPING = {
-    'dagger' : (1,4),
-    'club' : (1,4),
-    'spear' : (1,6),
-    'greatsword' : (2,6),
-    'halberd' : (1,10),
-    'rapier' : (1,8)
+    def get_attack_roll(self, attack:str, stat_bonus, target_ac):
+        stat_bonus_modifier = self.get_ability_modifier(self.abilities[stat_bonus])
+        if self.get_proficiency_bonus() + stat_bonus_modifier > target_ac:
+            return bob.player_attacks[attack](stat_bonus_modifier)
+
+    def get_initiative_roll(self):
+        return get_roll(1, 'd20') + self.get_ability_modifier('dexterity')
+
+DICE = {
+    'd4' : lambda : random.randint(1,4),
+    'd8' : lambda : random.randint(1,8),
+    'd10' : lambda : random.randint(1,10),
+    'd12' : lambda : random.randint(1,12),
+    'd20' : lambda : random.randint(1,20),
+    'd100' : lambda : random.randint(0,99)
 }
 
-hero = Entity('Gimli', 100, 'halberd', 12, 10, 10, 10, 10, 10, 10)
-hero.add_attack('Halberd', WEAPON_MAPPING['halberd'][0], WEAPON_MAPPING['halberd'][1], True)
+def get_roll(dice_amount, dice_type):
+        roll = 0
+        for dice in range(dice_amount):
+            roll += DICE[dice_type]()
+        return roll
 
+ATTACKS = {
+    'rend' : PhysicalAttack(1, 'd4').get_dmg_roll, 
+    'halberd' : PhysicalAttack(1, 'd10').get_dmg_roll, 
+    'greatsword' : PhysicalAttack(2, 'd6').get_dmg_roll
 
-print(hero.attacks)
+    }
+
+bob = Entity('bob', ['rend','halberd'], )
+
+opps = {
+    'jason' : Entity('jason', 10, ['rend'], 13, 14, 12),
+    'monkey' : Entity('monkey', 5, ['rend', 'dagger'], 10, 10, 10)
+}
+
+print(bob.player_attacks['rend'](bob.strength))
+print(bob.player_attacks['halberd'](bob.strength))
+
+print([1, 8, 4, 6, 3].sort())
+
 
