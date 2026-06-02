a0c274cc2ec0ee29993d10e14d87fd69269a480a
LukeAllen-2010 <lukeallen152@gmail.com>
2026-06-02 08:55:00 +0000

    update 5g game

--- Resources/oop/5g. OOP challenge/Entity.py
+++ Resources/oop/5g. OOP challenge/Entity.py
@@ -6,7 +6,8 @@ from get_roll import get_roll
 class Entity:
     def __init__(self, name:str, starting_health:int, player_attacks:list, armour_class:int, strength:int, dexterity:int, constitution:int=0, intelligence:int=0, wisdom:int=0, charisma:int=0, gp=0, sp=0, cp=0):
         self.name = name
-        self.__health = starting_health
+        self.level = 1
+        self.health = starting_health
         self.abilities = {'strength':strength, 'dexterity':dexterity, 'constitution':constitution, 'intelligence':intelligence, 'wisdom':wisdom, 'charisma':charisma}
         self.purse = {'gold':gp, 'silver':sp, 'copper':cp}
         self.ac = armour_class
@@ -26,40 +27,59 @@ class Entity:
     def get_proficiency_bonus(self):
         return (self.level - 1) // 4 + 2
     
-    def get_attack_roll(self, stat_bonus_modifier):
-        return self.get_proficiency_bonus() + stat_bonus_modifier
+    def get_attack_roll(self, ability_bonus_modifier):
+        return self.get_proficiency_bonus() + ability_bonus_modifier
 
     def attack_target(self, attack:str, target_ac):
-        ability_bonus = self.attacks[attack]
-        stat_bonus_modifier = self.get_ability_modifier(self.abilities[ability_bonus])
-        if self.get_attack_roll() > target_ac:
-            return bob.player_attacks[attack](stat_bonus_modifier)
+        ability_bonus_type = self.attacks[attack][1] # e.g dexterity or strength
+        print(ability_bonus_type)
+        print(self.abilities[ability_bonus_type])
+        ability_bonus_modifier = self.get_ability_modifier(ability_bonus_type) # finding bonus by passing stat type into get_ability_modifier()
+        if self.get_attack_roll(ability_bonus_modifier) > target_ac: 
+            return self.attacks[attack][0]() # runs get_damage_roll() for that attack and returns number
         print(f'{self.name} MISSED!!')
         return 0
 
     def get_initiative_roll(self):
         return get_roll(1, 'd20') + self.get_ability_modifier('dexterity')
 
-def get_roll(dice_amount, dice_type):
-        roll = 0
-        for dice in range(dice_amount):
-            roll += DICE[dice_type]()
-        return roll
-
-bob = Entity('bob', 10, ['rend', 'halberd'], 10, 10, 10)
-
-opps = {
+entities = {
     'jason' : Entity('jason', 10, ['rend'], 13, 14, 12),
-    'monkey' : Entity('monkey', 5, ['rend', 'greatsword'], 10, 10, 10)
+    'monkey' : Entity('monkey', 5, ['rend', 'greatsword'], 10, 10, 10),
+    'hero' : Entity('bob', 10, ['rend', 'halberd'], 10, 10, 10)
 }
 
-print(bob.attack_target('rend', 13))
+def get_initiative_order(entities):
+    initiative_order = {}
+    for entity in entities:
+        initiative_order[entity] = entities[entity].get_initiative_roll()
+    return {k : v for k, v in sorted(initiative_order.items(), key = lambda item : item[1])}
 
-print([1, 8, 4, 6, 3].sort())
+initiative_order = get_initiative_order()
 
-initiative_order = {}
+running = True
+while running:
+    for entity in initiative_order:
+        if entity == 'hero':
+            
 
-for opp in opps:
-    initiative_order[opp] = opps[opp].get_initiative_roll()
+        if entity.is_dead:
+            print(f'{entity.name} is dead')
+            initiative_order.remove(entity)
+        
+        else:
+            attack = entity.attack_target(random.choice(entity.attacks), entities['hero'].ac)
+            entities['hero'].health -= attack
+            if entities['hero'].health < 0:
+                print('YOU LOSE')
+                running = False
+            
+# enemies = ['troll_1', 'troll_2', 'goblin', 'bear', 'angry_toadstool']
 
-print(initiative_order)
+# enemy_list_str = f'Which enemy would you like to hit: '
+# for enemy_num, enemy in enumerate(enemies, 1):
+#     enemy_list_str += f'{enemy}({enemy_num}) '
+
+# player_target = int(input(enemy_list_str)) - 1
+# # target = int(input(f'which enemy would you like to hit: {enemies[0]}(1), {enemies[1]}(2)'))
+# player_target.get_roll(1,20)
--- Resources/oop/5g. OOP challenge/Main.py
+++ Resources/oop/5g. OOP challenge/Main.py
@@ -1,12 +1,4 @@
-# enemies = ['troll_1', 'troll_2', 'goblin', 'bear', 'angry_toadstool']
 
-# enemy_list_str = f'Which enemy would you like to hit: '
-# for enemy_num, enemy in enumerate(enemies, 1):
-#     enemy_list_str += f'{enemy}({enemy_num}) '
-
-# player_target = int(input(enemy_list_str)) - 1
-# # target = int(input(f'which enemy would you like to hit: {enemies[0]}(1), {enemies[1]}(2)'))
-# player_target.get_roll(1,20)
 
 # enemies[1]
 
@@ -54,60 +46,8 @@ class Weapon:
         else:
             print('Attack misses')
             return 0
-    
-def get_atk_roll(self, roll, target_ac, stat_bonus:str):
-    proficiency_bonus = self.weapon.proficiency_bonus
-    ability_modifier = get_ability_modifier(self.ability[stat_bonus]) #weapon's stat_bonus : str, dex, etc.
-    if roll + proficiency_bonus + ability_modifier > target_ac:
-        return self.rend.get_dmg_roll()
-    return False
-    
-
-class Entity:
-    def __init__(self, name:str, starting_health:int, weapon:str, armour_class:int, strength:int, dexterity:int, constitution:int, intelligence:int, wisdom:int, charisma:int, gp=0, sp=0, cp=0):
-        self.name = name
-        self.__health = starting_health
-        self.weapon_name = weapon
-        self.weapon_dice = WEAPON_MAPPING[weapon]
-        self.abilities = {'str':strength, 'dex':dexterity, 'con':constitution, 'int':intelligence, 'wis':wisdom, 'cha':charisma}
-        self.ac = armour_class
-
-    def get_roll(self, dice_amount, dice_type):
-        roll = 0
-        for dice in range(dice_amount):
-            roll += random.randint(1, dice_type)
-        return roll
-
-    def attack(self):
-        player_input = input('What attack do you want to use? Cast spell (1), hit with weapon (2) or do a backflip (3)')
-        x = getattr(self, player_input)
-        try:
-            x()
-        except:
-            print('Player input does not match attack options')
-
-def get_ability_modifier(self, ability_score):
-    ability_modifier = -5
-    for score in range(ability_score):
-        ability_modifier += 0.5
-    return int(round(ability_modifier))
 
 
-
-
-def get_roll(self, d_amt, d_type):
-    roll = 0
-    for i in range(d_amt):
-        roll += random.randint(1, d_type)
-    return roll
-
-
-# hero = Hero('Gimli', 100, 'halberd', 12, 10, 10, 10, 10, 10, 10)
-
-# hero.attack()
-
-
-
-class Humanoid(Entity):
-    def __init__(self, gold, silver, copper):
-        self.purse = {'gp':gold, 'sp':silver, 'cp':copper}
+# class Humanoid(Entity):
+#     def __init__(self, gold, silver, copper):
+#         self.purse = {'gp':gold, 'sp':silver, 'cp':copper}
